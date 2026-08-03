0c5534be83c8d3f8563e3af09534fc7e8ff13d42
x170doom <x170doom@gmail.com>
2026-08-03 17:20:35 +0100

    script renamed, bugfixes.
    fsuipc has a filename character limit i didnt know about so it couldnt detect the script.
    fatal error to do with a close bracket that went missing fixed
    more pain with the 777 door logic

--- WIP SCRIPTS/PMDGLVARSCRIPT.lua
+++ WIP SCRIPTS/PMDGLVARSCRIPT.lua
@@ -3,52 +3,55 @@
 --dev version for integration with pmdg 777,747,DC-8
 --todo:
 --remake aircraftcheck to support multiple aircraft types -done
---build event library calls into individual functions for each aircraft type
---rewrite/new functions for other aircraft types (find best method with least overhead)
+--build event library calls into individual functions for each aircraft type -done...ish
+--rewrite/new functions for other aircraft types (find best method with least overhead)- like that was ever going to happen, its as clean as i can get it
 --maybe some additional feedback in debugmode
 --fallbacks in situations where sim is unsure
---fix for issue #6 [no longer required. issue resolved by slc 1.6.6.9]
+--fix for issue #6 -[no longer required. issue resolved by slc 1.6.6.9]
+--see if i can make the 747 and 777 work with slc door arm/disarm behaviour (and cabin ready if possible)
 
 function initmain()
- local debugmode = true
- local seatbeltstate = "not yet set"
- local aircraftonground = true
- initarrays()
- aircraftcheck()
- initvar()
+	local debugmode = true
+	local seatbeltstate = "not yet set"
+	local aircraftonground = true
+	initarrays()
+	aircraftcheck()
+	initarray2()
 end
 
 
 function initarrays()--todo: 747 stuff
- local a = {}
- a["737"] = {}
- --a["747"] = {}
- a["777"] = {}
- a["dc-8"] = {}
- a["737"]["sboffset"] = 0x649F
- a["737"]["door2"] = 0x6C15
- a["737"]["door3"] = 0x6C1E
- a["737"]["door4"] = 0x6C1F
- --a["747"]["sboffset"] = 0x6C2B
- a["777"]["sboffset"] = 0x647B
- a["777"]["door2"] = 0x65DD
- a["777"]["door3"] = 0x65DE
- a["777"]["door4"] = 0x65DF
- a["777"]["door5"] = 0x65E0
- a["777"]["door6"] = 0x65E1
- a["777"]["door7"] = 0x65E2
- a["777"]["door8"] = 0x65E3
- a["777"]["door9"] = 0x65E4
- a["777"]["door10"] = 0x65E5
-end
-
-function initvar()
+	aircraftoffset = {}
+	aircraftoffset["737"] = {}
+	--aircraftoffset["747"] = {}
+	aircraftoffset["777"] = {} 
+	aircraftoffset["dc-8"] = {}
+	aircraftoffset["737"]["sboffset"] = 0x649F
+	aircraftoffset["737"]["door2"] = 0x6C15
+	aircraftoffset["737"]["door3"] = 0x6C1E
+	aircraftoffset["737"]["door4"] = 0x6C1F
+	--aircraftoffset["747"]["sboffset"] = 0x6C2B
+	aircraftoffset["777"]["sboffset"] = 0x647B
+	aircraftoffset["777"]["taxioffset"] = 0x648E
+	aircraftoffset["777"]["door2"] = 0x65DD
+	aircraftoffset["777"]["door3"] = 0x65DE
+	aircraftoffset["777"]["door4"] = 0x65DC--technically door 1 but the 777 door logic is cursed, see below in initarray2 for why
+	aircraftoffset["777"]["door5"] = 0x65E0
+	aircraftoffset["777"]["door6"] = 0x65E1
+	aircraftoffset["777"]["door7"] = 0x65E2
+	aircraftoffset["777"]["door8"] = 0x65E3
+	aircraftoffset["777"]["door9"] = 0x65E4
+	aircraftoffset["777"]["door10"] = 0x65E5
+	-----------------------------------above this line: aircraft offsets from sdk, below this line: other shit
 	aircraft_typelist = {
 		["PMDG 737"] = "737",
 		--["PMDG 747"] = "747",
 		["PMDG 777"] = "777",
 		--["PMDG DC-8"] = "DC-8"
 	}
+end
+
+function initarray2()-- function based arrays go here
 	doorlayout = {
 		"737" = function(offset,value)
 			if offset == 0x6C15 then
@@ -88,33 +91,33 @@ function initvar()
 				else
 					ipc.clearbitsUW("3367", 4)
 				end
-			elseif offset == 0x65DF then
+			elseif offset == 0x65DC then-- door 2l is technically exit 1 in sim. and it checks correctly. so to fix this 1L and 2L are treated as opposits, so on your layout door 2l should be door 0 and door 1l should be door 3. because doing this the easy way was never an option apparently
 				if value == 0 then
 					ipc.setbitsUW("3367", 8)
 				else
 					ipc.clearbitsUW("3367", 8)
 				end
-			elseif offset == 0x65E4 then
+			elseif offset == 0x65E4 and fivedoor then
 				local fivedoor = true
 				if value == 0 then
 					ipc.setbitsUW("3367", 64)
 				else
 					ipc.clearbitsUW("3367",64)
 				end
-			elseif offset == 0x65E5 then
+			elseif offset == 0x65E5 and fivedoor then
 				local fivedoor = true
 				if value == 0 then
 					ipc.setbitsUW("3367", 128)
 				else
 					ipc.clearbitsUW("3367",128)
 				end
-			elseif offset == 0x65E0 then
+			elseif offset == 0x65E0 and not fivedoor then
 				if value == 0 then
 					ipc.setbitsUW("3367", 16)
 				else
 					ipc.clearbitsUW("3367",16)
 				end
-			elseif offset == 0x65E1 then
+			elseif offset == 0x65E1 and not fivedoor then
 				if value == 0 then
 					ipc.setbitsUW("3367", 32)
 				else
@@ -131,6 +134,18 @@ function initvar()
 					ipc.setbitsUW("3367", 128)
 				else
 					ipc.clearbitsUW("3367",128)
+				end
+			elseif offset == 0x65E2 and fivedoor then
+				if value == 0 then
+					ipc.setbitsUW("3367", 16)
+				else
+					ipc.clearbitsUW("3367",16)
+				end
+			elseif offset == 0x65E3 and fivedoor then
+				if value == 0 then
+					ipc.setbitsUW("3367", 32)
+				else
+					ipc.clearbitsUW("3367",32)
 				end
 			else
 				debugfunction("doorcheck called without valid offset")
@@ -164,41 +179,45 @@ function autoseatbeltmaintain ()
 	end
 end
 
-function seatbeltcheck (offset, value)--initial implementation of array based offset logic, needs investigating if auto mode logic works with other types than 737
-	if offset == a[aircraft_type]["sboffset"] then
-		if value == 0 then
-			seatbeltstate = "off"
-			event.cancel(seatbeltcheck)
-			seatbeltsetstate(false)
-		elseif value == 1 then
-			seatbeltstate = "Auto"
-			if isaircraftonground == false then
-				if ipc.readSD(0x3324) < 10000 then
-					seatbeltsetstate(true)
-				elseif ipc.readSD(0x3324) > 10000 then
-					seatbeltstate(false)
+function seatbeltcheck (offset, value)
+	if aircraft_type ~= "777" then
+		if offset == aircraftoffset[aircraft_type]["sboffset"] then
+			if value == 0 then
+				seatbeltstate = "off"
+				event.cancel(seatbeltcheck)
+				seatbeltsetstate(false)
+			elseif value == 1 then
+				seatbeltstate = "Auto"
+				if isaircraftonground == false then
+					if ipc.readSD(0x3324) < 10000 then
+						seatbeltsetstate(true)
+					elseif ipc.readSD(0x3324) > 10000 then
+						seatbeltstate(false)
+					else
+						debugfunction("auto state init fail, altitude not defined")
+					end
+				event.timer(1000, "autoseatbeltmaintain")
 				else
-					debugfunction("auto state init fail, altitude not defined")
+					seatbeltsetstate(true)
 				end
-			event.timer(1000, "autoseatbeltmaintain")
-			else
+			elseif value == 2 then
+				seatbeltstate = "on" 
+				event.cancel(seatbeltcheck)
 				seatbeltsetstate(true)
+			else
+				debugfunction("seatbelt offset outside expected range")
 			end
-		elseif value == 2 then
-			seatbeltstate = "on" 
-			event.cancel(seatbeltcheck)
-			seatbeltsetstate(true)
+		elseif offset == 0x0366 then
+			if value == 1 then
+				local isaircraftonground = true
+			else
+				local isaircraftonground = false
+			end
 		else
-			debugfunction("seatbelt offset outside expected range")
-		end
-	elseif offset == 0x0366 then
-		if value == 1 then
-			local isaircraftonground = true
-		else
-			local isaircraftonground = false
+			debugfunction("offset not valid for seatbelt check")
 		end
 	else
-		debugfunction("offset not valid for seatbelt check")
+		debugfunction("777 actually uses the seatbelt avar correctly, ignoring")
 	end
 end
 
@@ -248,6 +267,16 @@ function doorcheck (offset,value)--much cleaner
 		debugfunction("door state cannot be determined due to invalid lights test switch position, offset ignored for now")
 	end
 
+function taxistate (offset,value)-- the random only light on the t7 that doesnt set its avar correctly
+	if value then
+		ipc.setbitsUW("0D0C", 8)
+	elseif not value then
+		ipc.clearbitsUW("0D0C", 8)
+	else
+		debugfunction("taxi light value was neither true or false...WTF?!?")
+	end
+end
+
 function debugfunction (errtext)
 	if debugmode then
 		ipc.log(errtext)
@@ -270,21 +299,22 @@ initmain()
 initevents()
 
 function initevents
-	event.offset(a[aircraft_type]["sboffset"], "UB", "seatbeltcheck")--seatbelt light
+	event.offset(aircraftoffset[aircraft_type]["sboffset"], "UB", "seatbeltcheck")--seatbelt light
 	event.offset(0x0366, "UB", "seatbeltcheck")--aircraftonground
-	event.offset(a[aircraft_type]["door2"], "UB", "doorcheck")--door2	
-	event.offset(a[aircraft_type]["door3"], "UB", "doorcheck")--door3
-	event.offset(a[aircraft_type]["door4"], "UB", "doorcheck")--door4
+	event.offset(aircraftoffset[aircraft_type]["door2"], "UB", "doorcheck")--door2	
+	event.offset(aircraftoffset[aircraft_type]["door3"], "UB", "doorcheck")--door3
+	event.offset(aircraftoffset[aircraft_type]["door4"], "UB", "doorcheck")--door4
+	if aircraft_type = "777" then --ofc the bloody t7 doesnt set the taxi AVAR, that would just make my life too easy wouldn't it
+		event.offset(aircraftoffset[aircraft_type]["taxioffset"], "UB", "taxistate")
+	end
 	--these should run regardless of aircraft
 	if largedoorcount then
-		event.offset(a[aircraft_type]["door5"], "UB", "doorcheck")--door5
-		event.offset(a[aircraft_type]["door6"], "UB", "doorcheck")--door6
-		event.offset(a[aircraft_type]["door7"], "UB", "doorcheck")--door7
-		event.offset(a[aircraft_type]["door8"], "UB", "doorcheck")--door8
-		event.offset(a[aircraft_type]["door9"], "UB", "doorcheck")--door9
-		event.offset(a[aircraft_type]["door10"], "UB", "doorcheck")--door10
+		event.offset(aircraftoffset[aircraft_type]["door5"], "UB", "doorcheck")--door5
+		event.offset(aircraftoffset[aircraft_type]["door6"], "UB", "doorcheck")--door6
+		event.offset(aircraftoffset[aircraft_type]["door7"], "UB", "doorcheck")--door7
+		event.offset(aircraftoffset[aircraft_type]["door8"], "UB", "doorcheck")--door8
+		event.offset(aircraftoffset[aircraft_type]["door9"], "UB", "doorcheck")--door9
+		event.offset(aircraftoffset[aircraft_type]["door10"], "UB", "doorcheck")--door10
 	end
 end
 --only run on aircraft with 5+ doors
--- these need adding to specialised function, first 3 should allways start and use the array for variables. the rest should only be called if enough doors are present
-
